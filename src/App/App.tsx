c58219617cc54359cf33dd9b789e7dc7942f5d24
Lewis <ljoncb@gmail.com>
2021-09-07 20:02:23 +0100

    Top bar mostly correct
    Might need to revisit the icons border thickness/colour

---
+++ src/App/App.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { FaRegHeart, FaHome } from "react-icons/fa";
+import "./App.scss";
+import lewis from "../assets/lewis.svg";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <div className='header-content'>
+          <span className='logo'>Boolgram</span>
+          <input className='search' type='text' placeholder='Search' />
+          <div className='actions'>
+            {/* TODO border might be too thick and colour might be off */}
+            <FaRegHeart size={25} />
+            {/* TODO colour is off */}
+            <FaHome size={25} />
+            <img
+              src={lewis}
+              className='rounded-img my-profile'
+              alt='my-profile'
+            />
+          </div>
+        </div>
+      </header>
+      <div className='content-container'>
+        <div className='feed'>feed column</div>
+        <div className='sidebar'>sidebar column</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
